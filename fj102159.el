d381904bcd170c0a0228550dfc80033abedcb29c
Frederic Jean <frederic.jean@sun.com>
2009-06-17 10:24:16 -0600

    Some random change that I can't remember....

--- fj102159.el
+++ fj102159.el
@@ -2,3 +2,9 @@
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (color-theme-twilight)
+
+(defun mac-toggle-max-window ()
+  (interactive)
+  (set-frame-parameter nil 'fullscreen (if (frame-parameter nil 'fullscreen)
+                                           nil
+                                           'fullboth)))
