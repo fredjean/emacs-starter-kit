710de1d3e72ea6be9d373f81ae88b739c07e6a8f
Frederic Jean <frederic.jean@sun.com>
2009-10-02 00:35:27 -0600

    Configuration changes and scala support.

--- init.el
+++ init.el
@@ -21,7 +21,6 @@
                     (or (buffer-file-name) load-file-name)))
 
 (add-to-list 'load-path dotfiles-dir)
-
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
