efd3db5469b8ef262fff3900d10d05f1aa70e957
Phil Hagelberg <technomancy@gmail.com>
2010-05-02 21:04:13 -0700

    Remove jabber.el; install it manually if you want it.
    I also recommend bitlbee and elim as alternatives. Hopefully the
    jabber.el developers can package it up properly in order to provide a
    better installation option.

--- init.el
+++ init.el
@@ -9,6 +9,12 @@
 ;; and brighter; it simply makes everything else vanish."
 ;; -Neal Stephenson, "In the Beginning was the Command Line"
 
+;; Turn off mouse interface early in startup to avoid momentary display
+;; You really don't need these; trust me.
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 ;; Load path etc.
 
 (setq dotfiles-dir (file-name-directory
@@ -16,8 +22,6 @@
 
 (add-to-list 'load-path dotfiles-dir)
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/jabber"))
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit/color-theme"))
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq package-user-dir (concat dotfiles-dir "elpa"))
