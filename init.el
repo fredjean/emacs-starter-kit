3cc50440d20cc21a08e8f95565e37f84131032c2
Phil Hagelberg <technomancy@gmail.com>
2010-07-10 15:04:07 -0700

    Some changes suggested by Bob Willan.

--- init.el
+++ init.el
@@ -21,6 +21,7 @@
                     (or (buffer-file-name) load-file-name)))
 
 (add-to-list 'load-path dotfiles-dir)
+
 (add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
 
 (setq autoload-file (concat dotfiles-dir "loaddefs.el"))
